a74f39d0f7ac9cf26f8820f97ac9581d4afa530b
Merlin Thomas <dev@emteeware.net>
2017-01-21 22:43:26 +0100

    Optimize imports

--- src/main/java/net/emteeware/FXMLController.java
+++ src/main/java/net/emteeware/FXMLController.java
@@ -1,16 +1,15 @@
 package net.emteeware;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
-
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.*;
 
 public class FXMLController implements Initializable {
 
